afd6d5d182f799d78a50cb0105ad4923e4b75b92
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2026-03-22 14:54:12 +0000

    replaced underscores for hyphens for all resources (#10)
    Co-authored-by: ryhhill1998 <ryhhill1998@outlook.com>

--- infrastructure/modules/ingestion/iam.tf
+++ infrastructure/modules/ingestion/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ingestion_lambda" {
-  name               = "${var.project_name}_ingestion_lambda_role_${var.environment}"
+  name               = "${var.project_name}-ingestion-lambda-role-${var.environment}"
   assume_role_policy = file("${path.module}/policies/lambda_trust_policy.json")
 }
 
