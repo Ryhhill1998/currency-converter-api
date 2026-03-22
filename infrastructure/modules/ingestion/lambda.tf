2b7c3b61c7f4d85eff2efb1414dd4d97d1933bf4
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2026-03-22 14:59:54 +0000

    updated policy and commented out lambda (#11)
    Co-authored-by: ryhhill1998 <ryhhill1998@outlook.com>

--- infrastructure/modules/ingestion/lambda.tf
+++ infrastructure/modules/ingestion/lambda.tf
@@ -1,17 +1,17 @@
-resource "aws_lambda_function" "ingestion_lambda" {
-  function_name = "${var.project_name}-ingestion-lambda-${var.environment}"
-  role          = aws_iam_role.ingestion_lambda.arn
-  package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.ingestion_lambda.repository_url}:latest"
-
-  timeout     = 30
-  memory_size = 128
-
-  architectures = ["arm64"]
-
-  lifecycle {
-    ignore_changes = [
-      image_uri,
-    ]
-  }
-}
+# resource "aws_lambda_function" "ingestion_lambda" {
+#   function_name = "${var.project_name}-ingestion-lambda-${var.environment}"
+#   role          = aws_iam_role.ingestion_lambda.arn
+#   package_type  = "Image"
+#   image_uri     = "${aws_ecr_repository.ingestion_lambda.repository_url}:latest"
+#
+#   timeout     = 30
+#   memory_size = 128
+#
+#   architectures = ["arm64"]
+#
+#   lifecycle {
+#     ignore_changes = [
+#       image_uri,
+#     ]
+#   }
+# }
