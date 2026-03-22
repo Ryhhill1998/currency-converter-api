d3028b91114564401f3e4be38b9a883a42780753
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2026-03-22 14:38:14 +0000

    initialised lambda handler func (#8)
    * initialised lambda handler func
    
    * move index to src
    
    * uv init
    
    * started adding iam
    
    * moved ecr lc policy to file
    
    * added lambda tf
    
    * updated names of all resources
    
    * updated dockerfile
    
    * updated lambda to run on python 3.14
    
    * tf fmt
    
    * added ingestion deploy wf
    
    * updated ecr.tf name
    
    * git status
    
    * updated ecr policy
    
    * updated ynl
    
    * fixed workflow
    
    * comm out for now
    
    * fixed fp
    
    * wf file name reformat
    
    ---------
    
    Co-authored-by: ryhhill1998 <ryhhill1998@outlook.com>

--- infrastructure/modules/ingestion/iam.tf
+++ infrastructure/modules/ingestion/iam.tf
@@ -1,1 +1,9 @@
+resource "aws_iam_role" "ingestion_lambda" {
+  name               = "${var.project_name}_ingestion_lambda_role_${var.environment}"
+  assume_role_policy = file("${path.module}/policies/lambda_trust_policy.json")
+}
 
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.ingestion_lambda.name
+}
--- infrastructure/modules/ingestion/lambda.tf
+++ infrastructure/modules/ingestion/lambda.tf
@@ -1,13 +1,17 @@
 # resource "aws_lambda_function" "ingestion_lambda" {
-#   function_name = "${var.project_name}-ingestion-${var.environment}"
+#   function_name = "${var.project_name}_ingestion_lambda_${var.environment}"
 #   role          = aws_iam_role.ingestion_lambda.arn
-#
 #   package_type  = "Image"
 #   image_uri     = "${aws_ecr_repository.ingestion_lambda.repository_url}:latest"
 #
-#   environment {
-#     variables = {
-#       S3_BUCKET_NAME = aws_s3_bucket.data_storage_bucket.id
-#     }
+#   timeout     = 30
+#   memory_size = 128
+#
+#   architectures = ["arm64"]
+#
+#   lifecycle {
+#     ignore_changes = [
+#       image_uri,
+#     ]
 #   }
 # }
