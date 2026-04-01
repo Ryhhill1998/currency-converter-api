57694b2032ee7ac8f349a34f9af5e260cff878e6
ryhhill1998 <ryhhill1998@outlook.com>
2026-04-01 07:19:33 +0100

    refactored index.py to use container

--- ingestion/src/index.py
+++ ingestion/src/index.py
@@ -1,25 +1,12 @@
-import httpx
+from src.container import Container
+from src.models.settings import GeneralSettings, get_general_settings
 
-from src.clients.ecb.ecb_client import EcbClient
-from src.clients.ecb.http_ecb_client import HttpEcbClient
-from src.models.settings import GeneralSettings, get_general_settings, LocalEcbSettings, get_local_ecb_settings, \
-    HttpEcbSettings, get_http_ecb_settings
-from src.services.ingestion_service import IngestionService
-from src.stores.archive.archive_store import ArchiveStore
-from src.stores.rates.rates_store import RatesStore
-
-client = httpx.AsyncClient()
+settings: GeneralSettings = get_general_settings()
 
 
 async def handler(_, __) -> dict[str, str]:
-    settings: GeneralSettings = get_general_settings()
-
-    ecb_client: EcbClient = select_ecb_client(settings.is_local_ecb_client)
-    archive_store: ArchiveStore = select_archive_store(settings.is_local_archive_store)
-    rates_store: RatesStore = select_rates_store(settings.is_local_rates_store)
-
-    ingestion_service = IngestionService(ecb_client=ecb_client, archive_store=archive_store, rates_store=rates_store)
-
-    await ingestion_service.run()
+    async with Container(settings) as container:
+        ingestion_service = container.get_ingestion_service()
+        await ingestion_service.run()
 
     return {"status": "success"}
