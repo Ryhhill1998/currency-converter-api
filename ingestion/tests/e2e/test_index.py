6bb5993a8a67ca0b85b046ce1f3d51eb14773613
ryhhill1998 <ryhhill1998@outlook.com>
2026-04-05 10:42:17 +0100

    mocked settings for e2e tests

---
+++ ingestion/tests/e2e/test_index.py
@@ -0,0 +1,21 @@
+from unittest.mock import patch
+
+import pytest
+
+from src.index import handler
+from src.models.settings import GeneralSettings
+
+
+@pytest.fixture
+def mock_settings() -> GeneralSettings:
+    return GeneralSettings(
+        is_local_ecb_client=True,  # make sure to not hit actual API
+        is_local_archive_store=False,  # store in mocked AWS resources
+        is_local_rates_store=False,  # store in mocked AWS resources
+    )
+
+
+@pytest.mark.asyncio
+async def test_handler(mock_settings: GeneralSettings) -> None:
+    with patch("src.index.get_general_settings", return_value=mock_settings):
+        await handler("", "")
