d8694c49abe80966daece20d4f1ae429d066dfef
ryhhill1998 <ryhhill1998@outlook.com>
2026-04-07 16:22:30 +0100

    updated tests

--- ingestion/tests/e2e/test_index.py
+++ ingestion/tests/e2e/test_index.py
@@ -5,6 +5,7 @@ import httpx
 from freezegun import freeze_time
 from mypy_boto3_s3.client import S3Client
 from pydantic import HttpUrl
+import polars as pl
 
 from src.index import handler
 from src.models.settings import Settings
@@ -12,8 +13,45 @@ from tests.e2e.constants import ARCHIVE_BUCKET_NAME
 
 
 @pytest.fixture
-def mock_ecb_data_path() -> str:
-    return "data/ecb/ecb_data.csv"
+def mock_ecb_data_df() -> pl.DataFrame:
+    return pl.DataFrame(
+        [
+            {
+                "CURRENCY": "B",
+                "CURRENCY_DENOM": "A",
+                "TIME_PERIOD": "2020-10-30",
+                "OBS_VALUE": 91.5953
+            },
+            {
+                "CURRENCY": "C",
+                "CURRENCY_DENOM": "A",
+                "TIME_PERIOD": "2026-03-27",
+                "OBS_VALUE": 1.6731
+            },
+            {
+                "CURRENCY": "D",
+                "CURRENCY_DENOM": "A",
+                "TIME_PERIOD": "2025-12-31",
+                "OBS_VALUE": 1.9558
+            },
+            {
+                "CURRENCY": "E",
+                "CURRENCY_DENOM": "A",
+                "TIME_PERIOD": "2026-03-27",
+                "OBS_VALUE": 19.2158
+            },
+        ]
+    )
+
+
+@pytest.fixture
+def mock_ecb_data_bytes(mock_ecb_data_df: pl.DataFrame) -> bytes:
+    return mock_ecb_data_df.write_csv().encode(encoding="utf-8")
+
+
+@pytest.fixture
+def mock_ecb_data_dicts(mock_ecb_data_df: pl.DataFrame) -> list[dict]:
+    return mock_ecb_data_df.to_dicts()
 
 
 @pytest.fixture
@@ -34,21 +72,18 @@ def mock_settings(mock_ecb_url: str) -> Settings:
 
 @pytest.fixture
 def mock_ecb_endpoint_response(
-    respx_mock, mock_ecb_url: str, mock_ecb_data_path: str
+    respx_mock, mock_ecb_url: str, mock_ecb_data_bytes: bytes
 ) -> Generator[None]:
-    with open(mock_ecb_data_path, "rb") as file:
-        ecb_data: bytes = file.read()
-
-    mock_response = httpx.Response(status_code=201, content=ecb_data)
+    mock_response = httpx.Response(status_code=201, content=mock_ecb_data_bytes)
     respx_mock.get(mock_ecb_url).mock(return_value=mock_response)
     yield
 
 
 @freeze_time("2026-01-01")
-def test_handler_stores_expected_archive_data(
+def test_handler_stores_expected_raw_data(
     mock_settings: Settings,
     mock_ecb_endpoint_response: None,
-    mock_ecb_data_path: str,
+    mock_ecb_data_dicts: list[dict],
     create_archive_bucket: None,
     s3_client: S3Client,
 ) -> None:
@@ -56,21 +91,38 @@ def test_handler_stores_expected_archive_data(
     handler("", "", settings=mock_settings)
 
     # ASSERT
-    with open(mock_ecb_data_path) as data_file:
-        expected_data = data_file.read()
-
     archive_file_path = "rates/type=raw/year=2026/month=01/day=01/raw_rates.csv"
-    actual_data = (
+    actual_data = pl.read_csv(
         s3_client.get_object(Bucket=ARCHIVE_BUCKET_NAME, Key=archive_file_path)["Body"]
         .read()
-        .decode(encoding="utf-8")
-    )
+    ).to_dicts()
 
-    assert actual_data == expected_data
+    assert actual_data == mock_ecb_data_dicts
+
+
+@freeze_time("2026-01-01")
+def test_handler_stores_expected_parsed_data(
+    mock_settings: Settings,
+    mock_ecb_endpoint_response: None,
+    mock_ecb_data_dicts: list[dict],
+    create_archive_bucket: None,
+    s3_client: S3Client,
+) -> None:
+    # ACT
+    handler("", "", settings=mock_settings)
+
+    # ASSERT
+    archive_file_path = "rates/type=parsed/year=2026/month=01/day=01/parsed_rates.json"
+    actual_data = pl.read_csv(
+        s3_client.get_object(Bucket=ARCHIVE_BUCKET_NAME, Key=archive_file_path)["Body"]
+        .read()
+    ).to_dicts()
+
+    assert actual_data == mock_ecb_data_dicts
 
 
 @pytest.mark.skip
-def test_handler_stores_expected_rates_data(
+def test_handler_stores_expected_live_data(
     mock_settings: Settings, create_archive_bucket: None
 ) -> None:
     handler("", "", settings=mock_settings)
